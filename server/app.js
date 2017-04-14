2c417c59deaf4ce2332e29b83d1493d80599e4c9
rahul <rahulmishra@adityassociates.com>
2017-04-14 23:28:31 +0530

    Basic Clean Ups for New {Project}

--- server/app.js
+++ server/app.js
@@ -3,9 +3,8 @@ var app = express();
 var router = require('./router.js');
 
 app.use(router);
-app.set('etag', 'strong');
 
-app.listen('3030', function(){
-	console.log('[ '+new Date+" ] Server Lisening To Port 3030");
+app.listen('8080', function(){
+	console.log('[ '+new Date+" ] Server Lisening To Port 8080");
 });
 
--- server/router.js
+++ server/router.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var promise = require('bluebird');
 var awsCon = require('./controllers/aws-con.js')
 var bodyParser = require('body-parser');
-var User = require('./controllers/UserController.js')
-var Heat = require('./controllers/HeatController.js')
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -17,50 +15,6 @@ router.all('/', function(req, res){
 
 //	User CRUD API Routes
 
-router.get('/users', User.find);
-router.post('/users', User.create);
 
-router.get('/heat', Heat.find);
-router.post('/report', Heat.report);
-router.post('/heat', Heat.create);
 
 module.exports = router;
-
-
-
-
-// var bodyParser.urlencoded({ extended: false })
-
-/*router.use(function(req, res, next){
-	console.log(router.param());
-	next();
-})*/
-/*
-router.all('/', function(req, res){
-	res.send({'msg': 'Nothing Here, Please Check The API Documentation.'});
-})
-
-router.get('/aws', function(req, res){
-	awsCon.upload(params, function(data){
-		res.send(data);
-	});	
-})
-
-router.post('/aws', JSONParser, function(req, res){
-	var params = req.body;
-	/*awsCon.upload(params, function(data){
-		res.send(data);
-	});	
-	res.send(params);
-})*/
-
-
-
-
-
-
-/*params = {
-	Bucket: 'dev-zobo-1/rahul-aws', 
-	Key: 'hello_world.txt',
-	Body: 'Hello World!'
-};*/
