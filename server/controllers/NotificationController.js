338ae1b6f6b8ae392b8c1b0ddf4514a148fae806
rahulmishra <rahulmishra.hello@gmail.com>
2017-08-17 14:59:27 +0530

    Revert "interim comit"
    This reverts commit fcff2f9cf12265b726588e9a9a0cd8107b4e1fa6.

---
+++ server/controllers/NotificationController.js
@@ -0,0 +1,83 @@
+var PubNub = require('pubnub');
+var rpio = require('rpio');
+
+function _lightLED(){
+    var pin = 12;		/* P12/GPIO18 */
+    var range = 1024;	/* LEDs can quickly hit max brightness, so only use */
+    var max = 128;		/*   the bottom 8th of a larger scale */
+    var clockdiv = 8;	/* Clock divider (PWM refresh rate), 8 == 2.4MHz */
+    var interval = 5;	/* setInterval timer, speed of pulses */
+    var times = 5;		/* How many times to pulse before exiting */
+    
+    /*
+     * Enable PWM on the chosen pin and set the clock and range.
+     */
+    rpio.open(pin, rpio.PWM);
+    rpio.pwmSetClockDivider(clockdiv);
+    rpio.pwmSetRange(pin, range);
+    
+    /*
+     * Repeatedly pulse from low to high and back again until times runs out.
+     */
+    var direction = 1;
+    var data = 0;
+    var pulse = setInterval(function() {
+        rpio.pwmSetData(pin, data);
+        if (data === 0) {
+            direction = 1;
+            if (times-- === 0) {
+                clearInterval(pulse);
+                rpio.open(pin, rpio.INPUT);
+                return;
+            }
+        } else if (data === max) {
+            direction = -1;
+        }
+        data += direction;
+    }, interval, data, direction, times);
+}
+
+module.exports = {
+    'sendNotification': function(req, res){
+        function publish() {
+            
+             pubnub = new PubNub({
+                 publishKey : 'pub-c-f8903dae-bcfa-4ff6-9702-d5c094e59118',
+                 subscribeKey : 'sub-c-140c3668-c679-11e6-bb2e-02ee2ddab7fe'
+             })
+                
+             function publishSampleMessage() {
+                 console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
+                 var publishConfig = {
+                     channel : "hello_world",
+                     message : "Hello from PubNub Docs!"
+                 }
+                 pubnub.publish(publishConfig, function(status, response) {
+                     console.log(status, response);
+                 })
+             }
+                
+             pubnub.addListener({
+                 status: function(statusEvent) {
+                     if (statusEvent.category === "PNConnectedCategory") {
+                         publishSampleMessage();
+                     }
+                 },
+                 message: function(message) {
+                     console.log("New Message!!", message);
+                     _lightLED();
+                 },
+                 presence: function(presenceEvent) {
+                     // handle presence
+                 }
+             })      
+             console.log("Subscribing..");
+             pubnub.subscribe({
+                 channels: ['hello_world'] 
+             });
+         };
+         publish()
+
+        res.send('Notification Sent!')
+    }
+}
